c6c4b331cd68a3d41560784dc8ea72072e123203
Szymon Rymarski <szymonrymarski@gmail.com>
2018-08-21 01:10:45 +0200

    change2

--- TreeSetIterator.java
+++ TreeSetIterator.java
@@ -1,5 +1,9 @@
+import java.util.Set;
+import java.util.TreeSet;
+
 public class TreeSetIterator {
     public static void maim(String[] args){
+        Set<Integer>numbers=new TreeSet<>();
 
     }
 
