c108299e3be02b92adf8fea9a107c31a03662be8
Szymon Rymarski <szymonrymarski@gmail.com>
2018-08-21 12:19:54 +0200

    next step

--- TreeSetIterator.java
+++ TreeSetIterator.java
@@ -4,6 +4,11 @@ import java.util.TreeSet;
 public class TreeSetIterator {
     public static void maim(String[] args){
         Set<Integer>numbers=new TreeSet<>();
+        numbers.add(45);
+        numbers.add(3);
+        numbers.add(21);
+        numbers.add(150);
+        numbers.add(1);
 
     }
 
