6414c4fefd96944538677d6b8a0f0424ebd42b46
Szymon Rymarski <szymonrymarski@gmail.com>
2018-08-30 17:16:24 +0200

    tutoriale

---
+++ lambda/SuppilerTest.java
@@ -0,0 +1,13 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SuppilerTest {
+
+    public static void main(String[]args){
+        List<Integer>ints=new ArrayList<>();
+        Random r=new Random()
+    }
+}
