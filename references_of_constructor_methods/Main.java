b7f2ee4f635acc685559614c4d3dd70f0948c13b
Szymon Rymarski <szymonrymarski@gmail.com>
2018-08-30 19:52:55 +0200

    tutoriale

--- references_of_constructor_methods/Main.java
+++ references_of_constructor_methods/Main.java
@@ -1,16 +1,29 @@
 package references_of_constructor_methods;
 
 import java.util.Arrays;
-
-public class Main {
-    public static void main(String[] args){
-        String[] names={"Kowalski","Abacki","Wojnarek","Bednarek","Czyż"};
-        Arrays.sort(names,Main::sortAscending);
-        consumeArray(names,System.out::println);
+import java.util.function.Consumer;
+public class Main{
+    public static void main(String[] args) {
+        String[] names = {"Kowalski", "Abacki", "Wojnarek", "Bednarek", "Krzywousty"};
+        Arrays.sort(names, Main::sortAscending);
+        consumeArray(names, System.out::println);
 
         System.out.println(">>>");
-        Arrays.sort(names,Main::sortDescending);
-        consumerArray(names,System.out::println);
+        Arrays.sort(names, Main::sortDescending);
+        consumeArray(names, System.out::println);
     }
 
+    private static <T> void consumeArray(T[] arr, Consumer<T> consumer) {
+        for (T t : arr) {
+            consumer.accept(t);
+        }
+    }
+
+    private static int sortAscending(String t1, String t2) {
+        return t1.compareTo(t2);
+    }
+
+    private static int sortDescending(String t1, String t2) {
+        return t2.compareTo(t1);
+    }
 }
